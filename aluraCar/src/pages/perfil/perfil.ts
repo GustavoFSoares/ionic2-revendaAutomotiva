e41e52788610df850d8d95f6ccf9c18793c2e8a0
Gustavo Soares <gustavo10.fsoares@gmail.com>
2017-09-29 20:22:13 -0300

    concluindo projeto

--- aluraCar/src/pages/perfil/perfil.ts
+++ aluraCar/src/pages/perfil/perfil.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-// import {  } from "module";
+import { UsuarioService } from "../../domain/usuario/usuario-service";
 
 @Component({
-  selector: 'page-perfil',
-  templateUrl: 'perfil.html'
+    selector: 'page-perfil',
+    templateUrl: 'perfil.html'
 })
 export class PerfilPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        private _service: UsuarioService,
+    ) { }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PerfilPage');
-  }
+    get usuarioLogado(){
+        return this._service.obtemUsuarioLogado();
+    }
 
 }
